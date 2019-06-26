f43faed2791c0bf71496b60cc64dd1ed29577dae
zhangcheng liang <zlian030@wch136-15.cs.ucr.edu>
2019-06-25 22:36:26 -0700

    Completed part 2

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: zlian030
  *      Partner(s) Name: Xiaojun He
  *  	Lab Section: 021
- *	Assignment: Lab #2  Exercise #1
+ *	Assignment: Lab #2  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,19 +13,43 @@
 #endif
 
 int main(void) {
-    DDRA = 0x00;
-    DDRB = 0xFF;
-    PORTA = 0x00;
-    PORTB = 0x00;
-    unsigned char temp_input = 0x00;
-    while (1) {
-	temp_input = PINA;
-	if(temp_input == 0x01) {
-	    PORTB = 0x01;
+    DDRA = 0x00; // Configure port A's 8 pins as inputs
+    DDRC = 0xFF; // Configure port B's 8 pins as outputs
+    PORTA = 0x00; // Initialize PORTB output to 0’s
+    PORTC = 0x00; // Initialize PORTB output to 0’s
+    unsigned char tmpA = 0x00;
+    unsigned char cntavail;
+    while(1){
+	tmpA = PINA;
+	cntavail = 4;
+	if ((tmpA & 0x01) == 1) {
+	    cntavail--;
 	}
-	else {
-	    PORTB = 0x00;
+	if ((tmpA & 0x02) == 1) {
+	    cntavail--;
+	}	    
+	if ((tmpA & 0x04) == 1) {
+	    cntavail--;
+	}	    
+	if ((tmpA & 0x08) == 1) {
+	    cntavail--;
+	}
+	if (cntavail == 4) {
+            PORTC = 0x04;
+	}
+	else if (cntavail == 3) {
+	    PORTC = 0x03;
+	}
+	else if (cntavail == 2) {
+  	    PORTC = 0x02;
+	}
+	else if (cntavail == 1) {
+	    PORTC = 0x01;
+	}
+	else{
+	    PORTC = 0x00;
 	}
     }
+
     return 1;
 }
