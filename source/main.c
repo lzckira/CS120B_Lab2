ca96a00c41ec6017e5d4bc7ff2570491637c3598
zhangcheng liang <zlian030@wch136-15.cs.ucr.edu>
2019-06-25 23:09:15 -0700

    updated

--- source/main.c
+++ source/main.c
@@ -20,13 +20,28 @@ int main(void) {
     unsigned char tmpA = 0x00;
     unsigned char tmpB = 0x00;
     unsigned char tmpC = 0x00;
+    unsigned char tmpD = 0x00;
+    unsigned short totalWeight = 0x0000;
+    unsigned char weightDiff = 0x00;
     while(1){
-	
-	
-	
-	
-	
-	
+	tmpA = PINA;
+	tmpB = PINB;
+	tmpC = PINC;
+	tmpD = 0x00;
+	totalWeight = tmpA + tmpB +tmpC;
+	if (totalWeight > 0x008C) {
+	    tmpD = tmpD | 0x01;
+	}
+	if (tmpA >= tmpC) {
+	    weightDiff = tmpA - tmpC;
+	}
+	else {
+	    weightDiff = tmpC - tmpA;
+	}
+	if (weightDiff > 0x50) {
+	   tmpD = tmpD | 0x02;
+	}
+	PORTD = tmpD;
     }
     return 1;
 }
